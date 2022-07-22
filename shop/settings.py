8463dfa582fda589f1d41ca60845e71b088fbfc1
admin <kuligturan@mail.ri>
2022-07-22 13:28:17 +0600

    fix

--- shop/settings.py
+++ shop/settings.py
@@ -13,6 +13,7 @@ import os
 from pathlib import Path
 from decouple import config
 import datetime
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -150,3 +151,4 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+django_heroku.settings(locals())
